6a44c85bf07c474b7f904a46ac27bf349b712f9c
capapa <safiev@gmail.com>
2023-03-20 00:21:28 +0500

    feat: add localStorage searchBar

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -2,12 +2,12 @@ function Card() {
   return (
     <div className="card">
       <div className="cardImg">
-        <img src="./public/1.jpg" alt="phone" />
+        <img src="./1.jpg" alt="phone" />
       </div>
       <p className="cardPrice">199.99 руб.</p>
       <p className="cardDescription">Обувь</p>
       <p className="cardRate">
-        <img src="./public/star.svg" alt="star" />
+        <img src="./star.svg" alt="star" />
         <span>5</span>
       </p>
       <p className="cardDelivery">Доставка завтра</p>
--- src/components/SearchBar.tsx
+++ src/components/SearchBar.tsx
@@ -1,12 +1,50 @@
-function SearchBar() {
-  return (
-    <div>
-      <form className="searchBar">
-        <input type="text" />
-        <button type="submit">Find</button>
-      </form>
-    </div>
-  );
+import { ChangeEvent, Component, FormEvent } from 'react';
+
+interface MyState {
+  findTextValue: string;
+}
+
+class SearchBar extends Component<MyState, MyState> {
+  constructor(props: MyState) {
+    super(props);
+    this.state = { findTextValue: '' };
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  componentDidMount() {
+    const findTextValue = localStorage.getItem('searchBar');
+    if (findTextValue) {
+      this.setState({ findTextValue });
+    }
+  }
+
+  handleChange(event: ChangeEvent<HTMLInputElement>) {
+    this.setState({ findTextValue: event.target.value });
+  }
+
+  handleSubmit(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    const { findTextValue } = this.state;
+    localStorage.setItem('searchBar', findTextValue);
+  }
+
+  render() {
+    const { findTextValue } = this.state;
+    return (
+      <div>
+        <form className="searchBar" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            value={findTextValue}
+            onChange={this.handleChange}
+          />
+          <button type="submit">Find</button>
+        </form>
+      </div>
+    );
+  }
 }
 
 export default SearchBar;
