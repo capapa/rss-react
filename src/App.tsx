d995ae7624e717c235d6c04a76a81b6d5790e378
capapa <safiev@gmail.com>
2023-03-19 14:07:37 +0500

    feat: add pages 404, about, add router

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,22 @@
+import { Route, Routes, Link } from 'react-router-dom';
+import NotFound from './pages/404';
+import Home from './pages/Home';
+import About from './pages/About';
+
 function App() {
-  return <h1>Hello, World!</h1>;
+  return (
+    <>
+      <header>
+        <Link to="/">Home</Link>
+        <Link to="/about">About us</Link>
+      </header>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="404" element={<NotFound />} />
+        <Route path="about" element={<About />} />
+      </Routes>
+    </>
+  );
 }
 
 export default App;
--- src/setupTests.ts
+++ src/setupTests.ts
@@ -1,5 +1,10 @@
 /* eslint-disable import/no-extraneous-dependencies */
 import matchers from '@testing-library/jest-dom/matchers';
-import { expect } from 'vitest';
+import { expect, afterEach } from 'vitest';
+import { cleanup } from '@testing-library/react';
 
 expect.extend(matchers);
+
+afterEach(() => {
+  cleanup();
+});
