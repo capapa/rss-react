87237719889cc64000a6591070a908a37b542f47
capapa <safiev@gmail.com>
2023-03-21 17:32:57 +0500

    fix: add current page in header, correct description, add test flag coverage

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -1,17 +1,49 @@
+import { Component } from 'react';
 import { Link, Outlet } from 'react-router-dom';
 
-function Layout() {
-  return (
-    <>
-      <header>
-        <Link to="/">Home</Link>
-        <Link to="/about">About us</Link>
-      </header>
-      <main>
-        <Outlet />
-      </main>
-    </>
-  );
+class Layout extends Component<object, object> {
+  render() {
+    let path;
+    switch (window.location.pathname.slice(1)) {
+      case '':
+        path = 'Home';
+        break;
+      case 'about':
+        path = 'About';
+        break;
+      default:
+        path = 'Not found';
+    }
+
+    return (
+      <>
+        <header>
+          <div className='header'>
+          <Link
+            to="/"
+            onClick={() => {
+              this.setState({});
+            }}
+          >
+            Home
+          </Link>
+          <Link
+            to="/about"
+            onClick={() => {
+              this.setState({});
+            }}
+          >
+            About us
+          </Link>
+          </div>
+          <div className="currentPage">Current page: {path}</div>
+        </header>
+        <main>
+          <Outlet />
+        </main>
+      </>
+    );
+  }
 }
 
 export default Layout;
